2e90a4a592a98a01b94e09cc4a5f4a5d0177bcd2
Shinjon Chakraborty <toshinjonc2006@gmail.com>
2024-05-03 17:43:01 +0530

    Add theme switch

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/game_8853236.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 const NavBar = () => {
   return (
     <>
       <HStack justifyContent="space-between" padding="5px 10px">
         <Image src={logo} boxSize="60px"></Image>
-        <Text> NavBar or smth</Text>
+        <ColorModeSwitch />
       </HStack>
     </>
   );
