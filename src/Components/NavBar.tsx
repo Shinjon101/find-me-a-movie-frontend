72fad6913f0a22ad792d28e43600a66a304b91ff
Shinjon Chakraborty <toshinjonc2006@gmail.com>
2024-05-03 16:57:36 +0530

    Modify Navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,9 +1,12 @@
-import { Text } from "@chakra-ui/react";
-
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/game_8853236.webp";
 const NavBar = () => {
   return (
     <>
-      <Text> NavBar or smth</Text>
+      <HStack justifyContent="space-between" padding="5px 10px">
+        <Image src={logo} boxSize="60px"></Image>
+        <Text> NavBar or smth</Text>
+      </HStack>
     </>
   );
 };
