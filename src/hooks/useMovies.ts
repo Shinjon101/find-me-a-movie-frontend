7eaaf9c5e3d8c3b501136890571e6217424e21c6
Shinjon Chakraborty <toshinjonc2006@gmail.com>
2024-05-05 15:09:41 +0530

    Add genre to response object

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -7,26 +7,57 @@ export interface Movie {
   title: string;
   poster_path: string;
   backdrop_path: string;
+  genres?: string[]; 
 }
 interface FetchMovieResponse {
   page: number;
   results: Movie[];
 }
-const useMovies = () =>{
-  
+interface Genre {
+  id: number;
+  name: string;
+}
+interface FetchMovieDetails {
+  genres: Genre[];
+}
+
+const useMovies = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
     const controller = new AbortController();
     apiClient
-      .get<FetchMovieResponse>("/popular", {signal: controller.signal}) 
-      .then((res) => setMovies(res.data.results))
+      .get<FetchMovieResponse>("/popular", { signal: controller.signal })
+      .then((res) => {
+        const resMovies = res.data.results;
+
+        const promises = resMovies.map((movie) =>
+          apiClient.get<FetchMovieDetails>(`/${movie.id}`)
+        );
+        Promise.all(promises)
+          .then((responses) => {
+            responses.forEach((response, index) => {
+              resMovies[index].genres = response.data.genres.map(
+                (genre) => genre.name
+              );
+            });
+            setMovies(resMovies);
+          })
+          .catch((error) => {
+            setError(error.message);
+          });
+      })
       .catch((err) => {
-        if(err instanceof CanceledError) return;
-        setError(err.message)});
-  return ()=> controller.abort();   
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort(); 
+    
   }, []);
- return {movies, error}
-}
+
+  return { movies, error };
+};
+
 export default useMovies;
