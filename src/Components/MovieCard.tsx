f1311d71415d9eb0b8cf5c39ccb1842d13d8a7d1
Shinjon Chakraborty <toshinjonc2006@gmail.com>
2025-06-18 19:14:55 +0530

    Implement middle/Ctrl+click support to movie cards

--- src/Components/MovieCard.tsx
+++ src/Components/MovieCard.tsx
@@ -1,5 +1,13 @@
-import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  HStack,
+  Heading,
+  Image,
+  LinkBox,
+} from "@chakra-ui/react";
 import Genres from "./Genres";
+import { Link as RouterLink } from "react-router-dom";
 import RatingScore from "./RatingScore";
 import { useNavigate } from "react-router-dom";
 import { imgExtractionUrl } from "../services/ImageExtractionUrl";
@@ -12,18 +20,24 @@ interface Props {
 const MovieCard = ({ movie }: Props) => {
   const navigate = useNavigate();
   return (
-    <Card onClick={() => navigate(`movies/${movie.id}`)}>
-      <Image src={imgExtractionUrl + movie.poster_path} />
-      <CardBody>
-        <Heading fontSize="2xl" marginBottom={3}>
-          {movie.title}
-        </Heading>
-        <HStack justifyContent="space-between" alignItems="flex-start">
-          <Genres movie={movie} />
-          <RatingScore score={movie.vote_average} />
-        </HStack>
-      </CardBody>
-    </Card>
+    <LinkBox
+      as={RouterLink}
+      to={`/movies/${movie.id}`}
+      _hover={{ textDecoration: "none" }}
+    >
+      <Card onClick={() => navigate(`movies/${movie.id}`)}>
+        <Image src={imgExtractionUrl + movie.poster_path} />
+        <CardBody>
+          <Heading fontSize="2xl" marginBottom={3}>
+            {movie.title}
+          </Heading>
+          <HStack justifyContent="space-between" alignItems="flex-start">
+            <Genres movie={movie} />
+            <RatingScore score={movie.vote_average} />
+          </HStack>
+        </CardBody>
+      </Card>
+    </LinkBox>
   );
 };
 
